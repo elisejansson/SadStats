e8e7665fb648c1a40d59992bed1b27a01a2fa5c6
cohenmatt <cohen1@student.unimelb.edu.au>
2017-05-14 18:24:13 +1000

    adding comments
    So essentially this Python script reads in our CSV file and outputs a JSON file. It needs a little tinkering because I don't know what format we need the JSON file in, but that should be easily worked out. 
    I'm still working out what to do with this JSON file once it's been generated. Is it interacted upon by JQuery? Not even sure how that works haha.

--- csv2json.py
+++ csv2json.py
@@ -1,7 +1,7 @@
 """ 
 	Title:			CSV to JSON converter.
 	Author: 		Matthew Cohen
-	Stdnt number:		763090
+	Stdnt number:	763090
 	Class:			INFO20001, Foundations of Informatics
 	Date: 			14/05/17
 """
@@ -12,14 +12,17 @@ import json
 csvfile = open('data.csv', 'r')
 jsonfile = open('jsondata.json', 'w')
 
-fieldnames = ("YEAR", "CAUSE_NAME", "STATE", "DEATHS", "AADR")
+fieldnames = ("YEAR", "CAUSE_NAME", "STATE", "DEATHS", "AADR")	# columns
 reader = csv.DictReader(csvfile, fieldnames)
 
-i = 0
-jsonfile.write("{")
+i = 0				# counter for number of data values
+jsonfile.write("{")	# starts parent object. Not sure if necessary 
+
+# iterates through every row and writes a new JSON object
 for row in reader:
-	jsonfile.write('"{:04d}":'.format(i))
+	jsonfile.write('"{:04d}":'.format(i))	# not sure if this step is necessary	
 	json.dump(row, jsonfile)
 	jsonfile.write(',\n')
 	i += 1
-jsonfile.write("}")
+	
+jsonfile.write("}")	# closes parent object
