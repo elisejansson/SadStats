77663f8f6a6249d266857e7f022f4322c5ffebe8
cohenmatt <cohen1@student.unimelb.edu.au>
2017-05-14 18:15:21 +1000

    Add files via upload

---
+++ csv2json.py
@@ -0,0 +1,25 @@
+""" 
+	Title:			CSV to JSON converter.
+	Author: 		Matthew Cohen
+	Stdnt number: 	763090
+	Class:			INFO20001, Foundations of Informatics
+	Date: 			14/05/17
+"""
+
+import csv
+import json
+
+csvfile = open('data.csv', 'r')
+jsonfile = open('jsondata.json', 'w')
+
+fieldnames = ("YEAR", "CAUSE_NAME", "STATE", "DEATHS", "AADR")
+reader = csv.DictReader(csvfile, fieldnames)
+
+i = 0
+jsonfile.write("{")
+for row in reader:
+	jsonfile.write('"{:04d}":'.format(i))
+	json.dump(row, jsonfile)
+	jsonfile.write(',\n')
+	i += 1
+jsonfile.write("}")
