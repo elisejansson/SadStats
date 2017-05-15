f334e1c030038b3df7124f79d1bdbd6a18376fac
Elise Jansson <elisejansson@Elises-MacBook-Pro.local>
2017-05-15 22:39:16 +1000

    csv file read in html

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+import csv
 from flask import Flask
 from flask import render_template #to combine data with html
 app = Flask(__name__)
 
+def get_csv():
+    csv_path = './data.csv'
+    csv_file = open(csv_path, 'rb')
+    csv_obj = csv.DictReader(csv_file)
+    csv_list = list(csv_obj) #to create permanent list
+    return csv_list
+
 @app.route("/pivottable")
 def pivottable():
     template = 'pivottable.html'
-    return render_template(template)
+    object_list = get_csv()
+    return render_template(template, object_list=object_list)
 
 if __name__ == '__main__':
     # Fire up the Flask test server
