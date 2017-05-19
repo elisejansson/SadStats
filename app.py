045db462e8fdc1357971233c7be684b0f087c5de
estoel <estoel@unimelb.student.edu.au>
2017-05-19 16:14:19 +1000

    adds pivot.py, changes to data, and pivot table api route

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import csv
 from flask import Flask
 from flask import render_template #to combine data with html
+#import pivot.py
+
 app = Flask(__name__)
 
 def get_csv():
@@ -8,13 +10,23 @@ def get_csv():
     csv_file = open(csv_path, 'rb')
     csv_obj = csv.DictReader(csv_file)
     csv_list = list(csv_obj) #to create permanent list
-    return csv_list
+    return csv_list   
+
+@app.route("/api/pivottable")
+def pivottable_api():
+    #csv_list should have been passed as a global variable
+    index = request.args.get('index')
+    values = request.args.get('values')
+    filtering = request.args.get('filtering')
+    pivot_data = pivot(index, values, filtering, csv_list)
+    #json_pivottable = pivot_data to json
+    #set mime type to application/json
+    return json_pivottable
 
 @app.route("/pivottable")
 def pivottable():
     template = 'pivottable.html'
-    object_list = get_csv()
-    return render_template(template, object_list=object_list)
+    return render_template(template)
 
 @app.route("/about")
 def about():
@@ -28,4 +40,6 @@ def findings():
 
 if __name__ == '__main__':
     # Fire up the Flask test server
+    csv_list = get_csv()
+    print csv_list
     app.run(debug=True, use_reloader=True)
