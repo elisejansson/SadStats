85f93ba95711788de88b6d4bce9253ac0f4a20fc
Elise Jansson <elisejansson@Elises-MBP.fritz.box>
2017-05-23 14:05:45 +1000

    changes for matt

--- pivot.py
+++ pivot.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import csv
+import seaborn as sns
 
 
 def get_csv():
@@ -99,16 +100,21 @@ def pivot(pt_index, pt_columns, pt_values, pt_filtering, pt_aggfunc):
     pivot_table = pd.pivot_table(df, index=pt_index, columns=pt_columns,
     values=pt_values, aggfunc=pt_aggfunc)
 
+    #cm = sns.light_palette("blue", as_cmap=True)
+
+    #pivot_table_f = pivot_table.round(2).style.background_gradient(cmap=cm)
+    #pivot_table_final = pivot_table_f.render()
+
     #pivot_table.rename_axis({'YEAR': 'Year', 'CAUSE_NAME':
     #'Cause', 'AADR': 'Age Adjusted Death Rate'}, inplace=True)
 
     html_pivottable = pd.DataFrame.to_html(pivot_table, buf=None, columns=None,
     col_space=None, header=True, index=True, na_rep='NaN', formatters=None,
     float_format=lambda x: '%10.2f' % x, sparsify=None, index_names=True, justify=None,
-    bold_rows=True, classes="table table-condensed", escape=True, max_rows=None, max_cols=None,
+    bold_rows=True, classes="pivottable", escape=True, max_rows=None, max_cols=None,
     show_dimensions=False, notebook=False, decimal='.', border=None)
 
     return html_pivottable
-    #print html_pivottable
+    #return pivot_table_final
 
 #pivot(["STATE"],["CAUSE_NAME"],["AADR"], {"YEAR":'None', "STATE":'None', "CAUSE_NAME": 'None'}, np.mean)
